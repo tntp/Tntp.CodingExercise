f20dc72c5b8a04285d6339696246b549045c8fd8
Rob Haden <rhaden@cfl.rr.com>
2018-08-13 20:26:23 -0400

    Set up dependency injection and JSON as the only content/media type.

--- Tntp.Solution/Tntp.WebApplication/App_Start/WebApiConfig.cs
+++ Tntp.Solution/Tntp.WebApplication/App_Start/WebApiConfig.cs
@@ -1,24 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Tntp.WebApplication.Models;
 
 namespace Tntp.WebApplication
 {
     public static class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
+        public static void Register(HttpConfiguration configuration)
         {
             // Web API configuration and services
+            var container = new Container();
+            container.Register<IRepository, DatabaseRepository>(new AsyncScopedLifestyle());
+            container.RegisterWebApiControllers(configuration);
+            container.Verify();
+
+            // We want JSON to be the only content/media type.
+            // In the future, we might not want to go through content negotiation at all - see https://www.strathweb.com/2013/06/supporting-only-json-in-asp-net-web-api-the-right-way/
+            // for implementation details.
+            configuration.Formatters.Clear();
+            configuration.Formatters.Add(new JsonMediaTypeFormatter());
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+            configuration.MapHttpAttributeRoutes();
         }
     }
 }
