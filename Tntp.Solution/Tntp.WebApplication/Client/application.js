6efe3cc8843a8c975bcf1ddc6f2861a9292751d9
Rob Haden <rhaden@cfl.rr.com>
2018-08-14 16:34:32 -0400

    Move the landing page to a Client subfolder. Move the custom JavaScript code into a separate file in the same locaiton.

---
+++ Tntp.Solution/Tntp.WebApplication/Client/application.js
@@ -0,0 +1,15 @@
+﻿angular.module('CommentsClient', []).controller('CommentsController', function($scope, $http) {
+    const BadRequestStatusCode = 400;
+
+    $http.get("http://localhost:61781/api/comments").then(function(response) {
+        $scope.commentList = response.data;
+    });
+
+    $scope.postComment = function() {
+        $http.post("http://localhost:61781/api/comments", { Username: $scope.username, Content: $scope.comment }).catch(function(response) {
+            if(response.status == BadRequestStatusCode) {
+                $scope.error = response.data.Message;
+            }
+        });
+    }
+});
