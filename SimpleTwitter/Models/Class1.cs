f5d7f27a243099c299b71ca07a3ddf891d3e4f14
lpmonda <43244248+lpmonda@users.noreply.github.com>
2018-09-15 22:12:52 -0500

    Adding Code Example

---
+++ SimpleTwitter/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleTwitter.Models
+{
+    public class FeedbackForm
+    {
+         
+            public string Name { get; set; }
+            public string text { get; set; }
+            //public string Phone { get; set; }
+           // public string Company { get; set; }
+           // public string AdditionalInformation { get; set; }
+            public HttpPostedFileBase ProjectInformation { get; set; }
+        
+    }
+}
